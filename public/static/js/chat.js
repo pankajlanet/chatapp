4f2a742e8b9cb7b15e9cffebb1d6e12234545ef2
anish <anis@somethignh.com>
2021-09-27 14:17:54 +0530

    added acknowledgement when sumbit the form

--- public/static/js/chat.js
+++ public/static/js/chat.js
@@ -1,6 +1,6 @@
 console.log("chat js is attached")
 
-const socket =  io();;
+const socket =  io();
 //welcome message after 2 sec when connection is established
 socket.on('Welcome' , (count)=> {
     const show = document.getElementById('show')
@@ -16,23 +16,77 @@ const usermsg = document.getElementById('usermsg')
 const chatbox = document.getElementById("chatbox")
 const submitmsg = document.getElementById('submitmsg');
 const output = document.getElementById('output')
+const userCount = document.getElementById('count')
+const exit = document.getElementById('exit')
+const submitLocation = document.getElementById('submitLocation')
 
+socket.on('userUpdate',(value)=> {
+
+    userCount.innerHTML = value
+
+})
+
+exit.addEventListener('click',()=>{
+    window.close()
+})
 
 //sumbit handler
  submitmsg.addEventListener('click',()=> {
-    const node = document.createElement('p')
-    node.innerHTML = usermsg.value;
-    chatbox.appendChild(node)
-    socket.emit('send',usermsg.value)
+    socket.emit('send',usermsg.value ,()=> {
+    } )
     
  })
 
- socket.on('send' ,(m)=> {
-     console.log(m)
+ socket.on('locationReceived',(locationLink)=> {
+    const node = document.createElement('a')
+    const textnode = document.createTextNode("User Location")
+    node.appendChild(textnode);
+    node.title="This is link"
+    node.href = locationLink
+    chatbox.appendChild(node)
+
  })
 
+ submitLocation.addEventListener('click',()=> {
+    if(!navigator.geolocation){
+        alert("geo location is not support by the browser")
+    }
+    else{
+        console.log(navigator.geolocation.getCurrentPosition((position)=> {
+                console.log(position.coords.latitude)
+
+                socket.emit('location',"https://www.google.com/maps?q="+position.coords.latitude+','+position.coords.longitude)
+        }))
+    }
+ })
+
+ 
+
  socket.on('received' , (m)=> {
-     console.log(m)
+
+    if(m === 'A new User have joined')
+    {
+        const node = document.createElement('p')
+        node.innerHTML = m;
+        node.style.color = 'green'
+        chatbox.appendChild(node)
+    }
+    else if(m === 'User left')
+    {
+        const node = document.createElement('p')
+        node.innerHTML = m;
+        node.style.color = 'red'
+        chatbox.appendChild(node)
+
+    }
+    else{
+        const node = document.createElement('p')
+        node.innerHTML = m;
+        chatbox.appendChild(node)
+    }
+
+
+    
  })
 
 // const buttonInc = document.getElementById('incr');
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require("path")
 const http = require('http')
 const socketio = require("socket.io")
+const Filter = require('bad-words')
 
 const app = express();
 const server = http.createServer(app)
@@ -11,19 +12,41 @@ const io = socketio(server)
 
 app.use(express.static(path.join(__dirname,'../public'))) // rendering the static webpages
 
-
+let personConnected = 0
 io.on("connection" ,(socket)=> {
+    ++personConnected;
+
+    console.log(personConnected)
     io.emit("Welcome" , 'Welcome to the Chat App')
     console.log("user connected")
+    socket.broadcast.emit('received', "A new User have joined")
+    socket.broadcast.emit('userUpdate' , personConnected);
     
 
-    socket.on('send' ,(e)=> {
+    socket.on('send' ,(e , callback)=> {
         console.log(e)
-        socket.emit('received', 'data received')
+        const filter = new Filter()
+        if(filter.isProfane(e))
+        {
+            io.emit('received', "Bad Words are not" )
+            callback()
+        }
+        else{
+            io.emit('received', e )
+        callback()
+        }
+        
     })
 
+    socket.on('location' ,(position)=> {
+        io.emit('locationReceived', position)
+
+    })
     socket.on('disconnect',()=> {
         console.log("user disconnnected")
+        socket.broadcast.emit('received', "User left")
+        --personConnected
+    socket.broadcast.emit('userUpdate' , personConnected);
     })
 })
 
@@ -44,4 +67,6 @@ app.get('/another' , (req,res)=> {
 
 server.listen(3000 , ()=> {
     console.log("Server is hosted on port : ", 3000)
+    
+    
 })
